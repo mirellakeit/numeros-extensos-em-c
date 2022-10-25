fc8f645fbd88be3d17c81d4ce3031d9c2f2f4d0f
Mirella Farben <mirellamt03@gmail.com>
2022-10-25 13:32:25 -0300

    Create cadeia.h

---
+++ cadeia.h
@@ -0,0 +1,10 @@
+int str_tamanho (char*);
+void str_copia (char*, char*);
+void str_concatena (char*, char*);
+char *unidade(int);
+char *dezena(int);
+char *centena(int);
+void retorna_numero_extenso(char*, int);
+int unidade_extensa(char*);
+int dezena_extensa(char*);
+int centena_extensa(char*);
